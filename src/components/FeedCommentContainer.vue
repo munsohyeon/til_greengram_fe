510f34fdbf9a21eb6c78e88b06d092031293e4d5
munsohyeon <msh1787@gmail.com>
2025-08-13 11:40:17 +0900

    댓글 더보기 수정

--- src/components/FeedCommentContainer.vue
+++ src/components/FeedCommentContainer.vue
@@ -37,6 +37,10 @@ const state = reactive({
     // ]
 });
 
+const data = {
+    rowPerPage: 31, 
+}
+
 //댓글 등록
 const onPostComment = async () => {
     if(state.comment.trim().length === 0) {
@@ -72,13 +76,15 @@ const getMoreComment = async () => {
     state.isLoading = true;
     const params = { 
         feed_id: props.feedId,
-        start_idx: state.commentList.length
+        start_idx: state.commentList.length,
+        size: data.rowPerPage
     }
     const res = await getCommentList(params)
     if(res.status === 200) {
-        const moreCommentList = res.data.result;
-        if(moreCommentList.length > 0) {
-            state.commentList = [...state.commentList, ...moreCommentList]
+        const result = res.data.result;
+        state.moreComment = result.moreComment;
+        if(result.commentList.length > 0) {
+            state.commentList.push(...result.commentList)
         }
     }
     state.isLoading = false;
