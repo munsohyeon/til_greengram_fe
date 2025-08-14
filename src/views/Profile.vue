6ad949181b550eea4778a2596f31a98cf78d938c
munsohyeon <msh1787@gmail.com>
2025-08-14 16:08:29 +0900

    프로필상태 기능 수정

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,7 +3,7 @@ import loadingImg from '@/assets/loading.gif';
 import ProfileImg from '@/components/ProfileImg.vue';
 import FeedCard from '@/components/FeedCard.vue';
 import { ref, reactive, onMounted, onUnmounted } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router';
 import { useAuthenticationStore } from '@/stores/authentication';
 import { getUserProfile, patchUserProfilePic } from '@/services/userService';
 import { postUserFollow, deleteUserFollow } from '@/services/followService';
@@ -11,16 +11,18 @@ import { getFeedList } from '@/services/feedService';
 import { bindEvent } from '@/utils/commonUtils';
 
 const fileInput = ref(null);
-
+const authenticationStore = useAuthenticationStore();
 const route = useRoute(); //PathVariable 데이터 가져오기 위한 용도
 
 const data = {
-    profileUserId: parseInt(route.params.userId),
+    profileUserId: 0,
     page: 1,
     rowPerPage: 20,
+
 }
 
 const state = reactive({
+    isMyProfile: false,
     isLoading: false,
     isFinish: false,
     userProfile: {
@@ -33,15 +35,36 @@ const state = reactive({
         allFeedLikeCount: 0,
         followerCount: 0,
         followingCount: 0,
+        followState: 0
     },
     list: []
 });
 
-const authenticationStore = useAuthenticationStore();
+const init = userId => {
+    state.isFinish = false;
+    state.userProfile = {
+        userId: 0,
+        uid: '',
+        pic: '',
+        nickName: '',
+        createdAt: '',
+        feedCount: 0,
+        allFeedLikeCount: 0,
+        followerCount: 0,
+        followingCount: 0,
+        followState: 0
+    }
+    state.list = []
 
+    data.page = 1;
+    data.profileUserId = userId;
+
+    state.isMyProfile = data.profileUserId === authenticationStore.state.signedUser.userId
+}
+
+init(parseInt(route.params.userId));
 console.log('route.params.userId:', route.params.userId);
 
-const isMyProfile = data.profileUserId === authenticationStore.state.signedUser.userId;
 
 /*
 팔로우 상태
@@ -62,8 +85,6 @@ const getFollowStateText = followState => {
             return '맞팔로우';
     }
 }
-
-
 
 const getUserData = async () => {
     const params = {
@@ -102,7 +123,7 @@ const removeUserPic = () => {
 }
 
 const onClickProfileImg = () => {
-    if (isMyProfile) {
+    if (state.isMyProfile) {
         fileInput.value.click();
     }
 }
@@ -124,14 +145,54 @@ const handlePicChanged = async e => {
 
 const handleScroll = () => { bindEvent(state, window, getFeedData) };
 
-onMounted(() => {
-    window.addEventListener('scroll', handleScroll);
+
+
+const getData = () => {
     getUserData();
     getFeedData();
+}
+
+//팔로우 버튼 클릭시
+const onClickFollow = async () => {
+
+    switch (state.userProfile.followState) {
+        case 0: case 2: //post            
+            const postRes = await postUserFollow({ toUserId: data.profileUserId });
+            if (postRes.status === 200) {
+                state.userProfile.followState += 1;
+                state.userProfile.followerCount += 1;
+            }
+            break;
+        default: //delete            
+            const deleteRes = await deleteUserFollow({ to_user_id: data.profileUserId });
+            if (deleteRes.status === 200) {
+                state.userProfile.followState -= 1;
+                state.userProfile.followerCount -= 1;
+            }
+            break;
+    }
+}
+
+
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+    getData();
 });
 
 onUnmounted(() => {
     window.removeEventListener('scroll', handleScroll);
+});
+
+onBeforeRouteUpdate((to, from) => {
+    console.log('onBeforeRouteUpdate: to', to);
+    console.log('to.params.userId:', to.params.userId);
+    const toUserId = parseInt(to.params.userId);
+    if (toUserId !== data.profileUserId) {
+        init(toUserId);
+
+        getData();
+    }
 });
 </script>
 
@@ -151,8 +212,9 @@ onUnmounted(() => {
                             <td class="pointer follower">{{ state.userProfile.followerCount }}</td>
                             <td class="pointer follow pl_10">팔로우</td>
                             <td class="pointer follow">{{ state.userProfile.followingCount }}</td>
-                            <td class="pl_10" v-if="!isMyProfile">
-                                <input type="button" class="instaBtn" :value="getFollowStateText('')" />
+                            <td class="pl_10" v-if="!state.isMyProfile">
+                                <input type="button" class="instaBtn"
+                                    :value="getFollowStateText(state.userProfile.followState)" @click="onClickFollow" />
                             </td>
                         </tr>
                     </tbody>
@@ -160,10 +222,11 @@ onUnmounted(() => {
 
                 <div>
                     <div class="d-inline-flex" @click="onClickProfileImg">
-                        <profile-img :clsValue="`profile ${isMyProfile ? 'pointer' : ''}`" :size="300"
+                        <profile-img :clsValue="`profile ${state.isMyProfile ? 'pointer' : ''}`" :size="300"
                             :pic="state.userProfile.pic" :userId="state.userProfile.userId" />
                     </div>
-                    <div className="d-inline-flex item_container width-50" v-if="isMyProfile && state.userProfile.pic">
+                    <div className="d-inline-flex item_container width-50"
+                        v-if="state.isMyProfile && state.userProfile.pic">
                         <i className="fa fa-minus-square color-red pointer" @click="removeUserPic" />
                     </div>
                     <input hidden type="file" accept="image/*" ref="fileInput" @change="handlePicChanged" />
